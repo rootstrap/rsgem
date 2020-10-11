18182dd58b044c91dfc96302ff20d9a84178e0ae
Jake Yesbeck <yesbeckjs@gmail.com>
2020-10-10 21:16:32 -0400

    Placeholder for lack of Github Email/Username
    When git isn't configured with an email or username, add a placeholder
    and issue a warning.
    
    Also make a spec that asserts this functionality is working.

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -55,6 +55,28 @@ RSpec.describe 'CLI' do
         expect(File.exist?("#{gem_name}/.github/workflows/ci.yml")).to eq true
       end
     end
+
+    context 'without github config properly set' do
+      let!(:previous_git_user_name) { `git config user.name`.strip }
+      let!(:previous_git_user_email) { `git config user.email`.strip }
+
+      before do
+        `git config --unset user.name`
+        `git config --unset user.email`
+      end
+
+      subject { `./exe/rsgem new #{gem_name} --ci=github_actions` }
+
+      it 'creates a new gem with placeholder information' do
+        expect(subject).to match(/Warning: No git username set, setting change_me for now/)
+        expect(subject).to match(/Warning: No git email set, setting change_me@notanemail.com for now/)
+      end
+
+      after do
+        `git config user.name #{previous_git_user_name}`
+        `git config user.email #{previous_git_user_email}`
+      end
+    end
   end
 
   describe 'rsgem new NAME --bundler=VALUE' do
